c06f05a869a0ad7fdc8ea0bde944ed034474a049
cristian2306 <cristian.castellanos@mail.escuelaing.edu.co>
2023-12-17 00:24:29 -0500

    - implements userRoutes - added requestUsers.rest

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,36 @@ import {
 import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
-
+// GET / api / users
+router.get('', (req, res) => {
+  const users = userService.getUsers();
+  res.send(users)
+})
+// GET / api / users /: id
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const user = userService.search({ id })
+  res.send(user)
+})
+// POST / api / users
+router.post('', (req, res) => {
+  const user = req.body
+  const createuser = userService.createUser(user)
+  res.send(createuser)
+})
+// PUT / api / users /: id
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  const user = req.body
+  const updateduser = userService.updateUser(id, user)
+  res.send(updateduser)
+})
+// DELETE / api / users /: id
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+  const deleteduser = userService.deleteUser(id)
+  res.send(deleteduser)
+})
 // TODO: Implement route controllers for user
 
 export { router };
